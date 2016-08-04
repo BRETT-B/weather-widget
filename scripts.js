72573b52d19724755467c0ee0854267d4446c8a4
Brett D Burdick <burdickbrett@outlook.com>
2016-08-04 10:45:11 -0400

    initial commit

--- scripts.js
+++ scripts.js
@@ -5,6 +5,17 @@ $(document).ready(function(){
 	var canvas = document.getElementById('currentTemp');  //local variables to doc ready function and its children
 	var context = canvas.getContext('2d');
 	var currentTemp = 0;
+	var geo = false;
+	$('.geoLocate').click(function(){
+		event.preventDefault();
+		geo = true;
+		checkLocation();
+	})
+	function checkLocation(){
+		if(geo){
+			
+		}
+	}
 	$('.weatherForm').submit(function(){
 		// keep the form from submitting
 		event.preventDefault();
@@ -16,25 +27,32 @@ $(document).ready(function(){
 		$.getJSON(url, function(weatherData){
 			// set up a variable for the user's city's temp
 			currentTemp = weatherData.list[0].main.temp;
+			console.log(weatherData.list[0]);
 			animate(0);
 		}); //end of JSON function
 	}); //end of weather form submission
 	function animate(current){
-		var deg = '\u00b0';
-		var tempColor = '#FF0000'; //used on outside arc
 		context.clearRect(0,0,300,300);
+		var deg = '\u00b0';
+		// ==============================
+		// 		GRADIENT COLORS
+		// ==============================
+		var tempColor = context.createLinearGradient(0, 0, canvas.width, 0);
+		tempColor.addColorStop(0.15, "magenta");
+		tempColor.addColorStop(0.3, "red");
+		tempColor.addColorStop(0.7, "blue");
 		// ==============================
 		// 		TEMPERATURE TEXT
 		// ==============================
 		context.font="16px Arial";
-		context.fillText((Math.floor(currentTemp)+deg+' F'),137,108);
+		context.fillText((Math.round(currentTemp)+deg+' F'),137,108);
 		// ===============================
 		// 			INSIDE ARC
 		// ===============================		
 		context.beginPath();
 		context.lineWidth = 30;
 		context.arc(155,100,45, Math.PI * 0, Math.PI * 2);
-		context.strokeStyle = '#8ee9f2';
+		context.strokeStyle = '#00ffbb';
 		context.stroke();
 		// ===============================
 		// 			OUTSIDE ARC
